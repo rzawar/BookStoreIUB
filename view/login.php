20c285d6189db9b7030c2245dfdb4e6fb545e93b
Rohit Zawar <zawar.rohit@gmail.com>
2014-03-19 20:08:53 -0400

    making UI changes for register User

--- view/login.php
+++ view/login.php
@@ -65,11 +65,112 @@ include '../model/Login.php';
         </label>
         <button class="btn btn-lg btn-primary btn-block" type="submit" name = "submit" >Sign in</button>
 		<h5 class="text-muted">New to IUBookshelf ?</h5>
-		<button class="btn btn-lg btn-success btn-block" type="submit" name = "submit" value ="signin">Create Account</button>
+		<button class="btn btn-lg btn-success btn-block" data-toggle="modal" data-target="#registerModal">Create Account</button>
       </form>
-
+	<button class="btn btn-lg btn-success btn-block" data-toggle="modal" data-target="#registerModal">Create Account</button>
 	 
     </div> <!-- /container -->
+	<!-- the registration modal that pops up -->
+	<div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="registerModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title" id="registerModalLabel">New Registration</h4>
+				</div>
+					
+				<div class="modal-body">
+					<div class="container" style="margin: 10px;">
+						
+						<!-- New user to fill out basic profile information -->
+						
+							<div id="poslabel" class="control-group">
+								<label class="control-label" for="inputUserName">User Name</label>
+								<div class="controls">
+									<input name="user_name" type="text" id="user_name" class="required username" minlength="5" >
+									<input name="btnAvailable" type="button" id="btnAvailable" value="Check Availability" class="btn btn-warning">
+								<br>
+								<span style="color:red; font: bold 10px verdana; " id="checkid"  ></span> 
+								</div>
+                            </div>
+							
+							<div id="poslabel" class="control-group">
+                                 <label class="control-label" for="inputFirstName">First Name </label>
+                                 <div class="controls">
+									<input name="first_name" type="text" id="first_name" size="40" class="required">
+                                 </div>
+                            </div>
+							
+							<div id="poslabel" class="control-group">
+                                 <label class="control-label" for="inputLastName">Last Name </label>
+                                 <div class="controls">
+									<input name="last_name" type="text" id="last_name" size="40" class="required">
+                                 </div>
+                            </div>
+
+                            <div id="poslabel" class="control-group">
+                                 <label class="control-label" for="inputEmail">Email</label>
+                                <div class="controls">
+									<input name="usr_email" type="text" id="usr_email" class="required email"> 
+									<span class="example">** Please enter a valid email address</span>
+								</div>
+                            </div>
+							
+                            <div  id="poslabel"  class="control-group">
+                                <label class="control-label" for="inputPassword">Password</label>
+                                <div class="controls">
+									<input name="pwd" type="password" class="required password" minlength="5" id="pwd"> 
+									<span class="example">** Minimum 5 characters</span>
+								</div>
+                            </div>
+                               
+                            <div id="poslabel"  class="control-group">
+                                <label class="control-label" for="inputPassword">Re-enter Password</label>
+                                <div class="controls">
+									<input name="pwd2"  id="pwd2" class="required password" type="password" minlength="5" equalto="#pwd">
+								</div>
+                            </div>
+							
+							<div id="poslabel" class="control-group">
+                                 <label class="control-label" for="inputAddress">Address</label>
+                                 <div class="controls">
+									<!--<input name="address" type="text" id="address" size="80">-->
+									<textarea class="input-xlarge" name="address" id="address" rows="2" cols="70"></textarea>
+                                 </div>
+                            </div>
+							
+							<div id="poslabel" class="control-group">
+                                 <label class="control-label" for="phoneno">Phone no.</label>
+                                 <div class="controls">
+									<input name="phoneno" type="text" id="phoneno" size="20" class="required">
+                                 </div>
+                            </div>
+							
+							<div id="poslabel" class="control-group">
+                                 <label class="control-label" for="dob">Date of Birth</label>
+                                 <div class="controls">
+									<input name="dob" type="text" id="dob" size="10" class="required">
+                                 </div>
+                            </div>
+                               
+                            <div id="poslabel"  class="control-group">
+								<div class="controls"><br>
+                                <button type="submit" class="btn btn-success" name="doRegister" type="submit" id="doRegister" value="Register">Register</button>
+                                <button type="reset" class="btn">Clear</button>
+                                </div>
+                            </div>
+					</div>
+				</div>
+				
+				<!-- footer with 'Close' and 'Save Changes' tags
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				</div>
+				-->
+			</div>
+		</div>
+	</div>
 	<hr class="featurette-divider">
 
       <!-- /END THE FEATURETTES -->
@@ -84,6 +185,44 @@ include '../model/Login.php';
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+   <script src="../bootstrap-3.1.1/dist/js/bootstrap.min.js"></script>
+   <script src="../bootstrap-3.1.1/docs/assets/js/docs.min.js"></script>
+	<script src="../bootstrap-3.1.1/js/modal.js"></script>
+	<script>
+		$(document).ready(function() {
+			$("#doRegister").click(function() {
+			//alert("here button clicked");
+			$.ajax({
+			type : "GET",
+			url: 'process.php',
+			//data: 'title='+notetitle+'contents='+notecontents,
+			//data:{title:"notetitle", contents:"notecontents"},
+			success: function(data){
+				alert("success!!!"+data);
+				location.reload(true);
+				// Load the content in to the page.
+				},
+			error: function(jqXHR, exception) {
+            if (jqXHR.status === 0) {
+                alert('Not connect.\n Verify Network.');
+            } else if (jqXHR.status == 404) {
+                alert('Requested page not found. [404]');
+            } else if (jqXHR.status == 500) {
+                alert('Internal Server Error [500].');
+            } else if (exception === 'parsererror') {
+                alert('Requested JSON parse failed.');
+            } else if (exception === 'timeout') {
+                alert('Time out error.');
+            } else if (exception === 'abort') {
+                alert('Ajax request aborted.');
+            } else {
+                alert('Uncaught Error.\n' + jqXHR.responseText);
+            }
+        }
+		});
+    });
+});
+	</script>
   </body>
 </html>
---
+++ view/process.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+/*
+mysql_connect("localhost", "root", "");
+mysql_select_db("iubookshelf");
+
+if(isset($_GET['data1'])){
+
+$username = $_GET['data1'];
+$fname = $_GET['data2'];
+/*
+echo $lname = $_GET['lname'];
+echo $email = $_GET['email'];
+echo $password = $_GET['password'];
+echo $address = $_GET['address'];
+echo $phoneno = $_GET['phoneno'];
+echo $dob = $_GET['dob'];
+$sql_insert = "INSERT INTO `users` VALUES ('$username', '$fname', 'patel', 'abdpatel@indiana.edu', '13456', 'kurla', '98767878', '06/17/1986');";
+//	$sql_insert = "INSERT INTO `users` VALUES ($username, $fname, $lname, $email, $password, $address, $phoneno, $dob);";
+echo mysql_query($sql_insert) or die("Insertion Failed:" . mysql_error()); */
+exit();
+}
+?>
